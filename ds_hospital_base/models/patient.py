1ef012aad6ced1ceafff2d530b518efb6acad710
Rizki Desu <s4y4pu1an6@gmail.com>
2025-01-26 16:04:29 +0700

    add scurity and add res parner

--- ds_hospital_base/models/patient.py
+++ ds_hospital_base/models/patient.py
@@ -7,3 +7,11 @@ class Patient(models.Model):
     # INHERITS RES PARTNER
     _inherits       = {'res.partner': 'partner_id'}
     partner_id      = fields.Many2one('res.partner', string='Patient', required=True, ondelete='cascade')
+
+    @api.model
+    def create(self, vals):
+        res = super(Patient, self).create(vals)
+        res.partner_id.write({'hospital_partner_type': 'patient', 'company_type': 'person', 'is_company': False})
+        return res
+    
+    responsible_patient = fields.Many2one('res.partner', string='Responsible Patient', domain=[('company_type', '=', 'person')])
